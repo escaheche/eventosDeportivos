8e6148d8868e72fbf06fae653833de824bfa5b8f
Eduardo Molina <emolina@otschile.clm>
2024-07-27 00:45:19 -0300

    Ajuste en setup para levantar el proyecto en Sonar

--- setup-sonarqube.sh
+++ setup-sonarqube.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Variables de entorno
-SONARQUBE_URL="http://localhost:9000"
+SONARQUBE_URL="http://sonarqube:9000"
 SONARQUBE_TOKEN="sqb_f5057b641e39f85b29c33633eaf832ab3410b8ba"
 PROJECT_KEY="EventosDeportivosTest"
 PROJECT_NAME="EventosDeportivosTest"
 GITHUB_APP_ID="954236"
 GITHUB_APP_SECRET="bb2ab547deb3c3ededa1e26fd0dbf15f31fcdf86"
-GITHUB_APP_PRIVATE_KEY=$(cat /private_key.pem)
+GITHUB_APP_PRIVATE_KEY=$(cat /usr/local/bin/private_key.pem)
 
 # Función para verificar si SonarQube está listo
 function check_sonarqube {
@@ -21,27 +21,38 @@ function check_sonarqube {
 # Llamar a la función de verificación
 check_sonarqube
 
+# Función para realizar una solicitud curl y verificar el código de respuesta
+function curl_with_check {
+  RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -u $SONARQUBE_TOKEN: "$@")
+  if [ "$RESPONSE" -eq 200 ]; then
+    echo "Solicitud exitosa: $@"
+  else
+    echo "Error en la solicitud: $@. Código de respuesta: $RESPONSE"
+    exit 1
+  fi
+}
+
 # Crear el proyecto en SonarQube
 echo "Creando el proyecto en SonarQube..."
-curl -u $SONARQUBE_TOKEN: -X POST "$SONARQUBE_URL/api/projects/create" \
+curl_with_check -X POST "$SONARQUBE_URL/api/projects/create" \
   -d "name=$PROJECT_NAME" \
   -d "project=$PROJECT_KEY"
 
 # Configurar la integración de GitHub
 echo "Configurando la integración de GitHub..."
-curl -u $SONARQUBE_TOKEN: -X POST "$SONARQUBE_URL/api/settings/set" \
+curl_with_check -X POST "$SONARQUBE_URL/api/settings/set" \
   -d "key=sonar.pullrequest.github.repository" \
   -d "value=escaheche/eventosDeportivos"
 
-curl -u $SONARQUBE_TOKEN: -X POST "$SONARQUBE_URL/api/settings/set" \
+curl_with_check -X POST "$SONARQUBE_URL/api/settings/set" \
   -d "key=sonar.pullrequest.github.endpoint" \
   -d "value=https://api.github.com"
 
-curl -u $SONARQUBE_TOKEN: -X POST "$SONARQUBE_URL/api/settings/set" \
+curl_with_check -X POST "$SONARQUBE_URL/api/settings/set" \
   -d "key=sonar.pullrequest.github.app.id" \
   -d "value=$GITHUB_APP_ID"
 
-curl -u $SONARQUBE_TOKEN: -X POST "$SONARQUBE_URL/api/settings/set" \
+curl_with_check -X POST "$SONARQUBE_URL/api/settings/set" \
   -d "key=sonar.pullrequest.github.app.privateKey.secured" \
   -d "value=$GITHUB_APP_PRIVATE_KEY"
 
