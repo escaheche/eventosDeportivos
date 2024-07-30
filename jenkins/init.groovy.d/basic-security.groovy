0ace91dc33fe04a2d26c550468858c7feb322450
Eduardo Molina <emolina@otschile.clm>
2024-07-29 22:19:49 -0300

    ajuste para error de sonarGlobalConfig

--- jenkins/init.groovy.d/basic-security.groovy
+++ jenkins/init.groovy.d/basic-security.groovy
@@ -4,22 +4,19 @@ import jenkins.install.InstallState
 import com.cloudbees.plugins.credentials.*
 import com.cloudbees.plugins.credentials.domains.*
 import com.cloudbees.plugins.credentials.impl.*
-import org.sonarsource.scanner.jenkins.SonarGlobalConfiguration
-import org.sonarsource.scanner.jenkins.SonarQubeInstallation
 
-// Leer las variables de entorno
 def sonarUsername = System.getenv("SONARQUBE_CREDENTIALS_USR")
 def sonarPassword = System.getenv("SONARQUBE_CREDENTIALS_PSW")
 
 Thread.start {
-    sleep(20000)
+    sleep(30000) // Esperar 30 segundos para asegurarse de que los plugins estén cargados
 
     def instance = Jenkins.getInstance()
     instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
 
     // Crear usuario administrador
     def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-    hudsonRealm.createAccount(sonarUsername,sonarPassword)
+    hudsonRealm.createAccount(sonarUsername, sonarPassword)
     instance.setSecurityRealm(hudsonRealm)
     instance.save()
 
@@ -30,22 +27,34 @@ Thread.start {
         CredentialsScope.GLOBAL,
         "sonarqube-credentials", // Este ID es referenciado en la configuración de SonarQube
         "SonarQube Credentials",
-        sonarUsername, // Reemplaza con tu usuario de SonarQube
-        sonarPassword  // Reemplaza con tu contraseña de SonarQube
+        sonarUsername,
+        sonarPassword
     )
     credentialsStore.addCredentials(domain, credential)
 
-    // Configuración de SonarQube
-    def sonarQubeInstallation = new SonarQubeInstallation(
-        "SonarQube", 
-        "http://sonarqube:9000", 
-        null, 
-        "sonarqube-credentials", 
-        ""
-    )
-    def sonarGlobalConfig = SonarGlobalConfiguration.get()
-    sonarGlobalConfig.setInstallations(sonarQubeInstallation)
-    sonarGlobalConfig.save()
+    // Esperar a que el plugin de SonarQube esté disponible
+    def sonarGlobalConfig = null
+    while (sonarGlobalConfig == null) {
+        try {
+            sonarGlobalConfig = org.sonarsource.scanner.jenkins.SonarGlobalConfiguration.get()
+            if (sonarGlobalConfig != null) {
+                def sonarQubeInstallation = new org.sonarsource.scanner.jenkins.SonarQubeInstallation(
+                    "SonarQube", 
+                    "http://sonarqube:9000", 
+                    null, 
+                    "sonarqube-credentials", 
+                    ""
+                )
+                sonarGlobalConfig.setInstallations(sonarQubeInstallation)
+                sonarGlobalConfig.save()
+                println("SonarQube plugin configured successfully.")
+            }
+        } catch (Exception e) {
+            println("SonarQube plugin not available yet, retrying in 5 seconds...")
+            e.printStackTrace()
+            sleep(5000)
+        }
+    }
 
     instance.save()
 }
